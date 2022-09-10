0f00fa70ce4b2c661974a82c38ba0d094ec9e7a7
momin-abdurraheman <abdurjmi1923@gmail.com>
2022-09-10 17:09:51 +0530

    Started using express

--- server.js
+++ server.js
@@ -1,68 +1,70 @@
-//Package import-->this http package means u need a server of 
-// http type and then we r storing this package in a variable 
-// for further use 
-const http = require('http');
+const express = require('express');
 
-// This server will communicate at port 8081
-const port = 8081;//Local port number
+//Initialization
+//Store all functionality of express in single variable so with the 
+//help of it we can do all functions of express.
 
-// It is list we want to print 
-const toDOList = ["Complete Node Byte", "Play Cricket"];
-http
-    .createServer((req, res) => {
-        const { method, url } = req;
-        if (url === "/todos") {
-            if (method === "GET") {
-                res.writeHead(200);
-                res.write(toDOList.toString());
+let app = express();
 
-            }
-            else if (method === "POST") {
-                let body = "";
-                req.on('error', (err) => {
-                    console.error(err);
-                }).on('data', (chunk) => {//Data travels in form of chunk(pieces of JSON file) not in JSON file so that bugs can be easily fixed also it is also better in terms of speed.
-                    // SO now when we recieve pieces of chunk from browser to mein server we have to assemble them again in so we r storing in body variable below is code for that
-                    body += chunk;
-                    console.log(chunk);
-                }).on('end', () => { //After the process is end we copy the body data into JSON File
-                    body = JSON.parse(body);
-                    console.log("Data : ", body);
-                })
-            }
-            else {
-                res.writeHead(501);
-            }
+//Whatever transfer of data will happen between server and browser
+//it will be of type JSON
+
+app.use(express.json());
+
+
+const port = 8081;
+
+const toDoList = ["Complete Node Byte ", "Play Cricket "];
+
+//I want to make a get request that should be served as 
+// /todos like https://Localhost:8081/todos
+
+app.get("/todos", (req, res) => {
+    //Callback i.e whenever someone make a get request we will have
+    // request data and we will send some response
+    res.status(200).send(toDoList);
+})
+//http://Localhost:8081/todos
+app.post("/todos", (req, res) => {
+    // Callback function
+    let newToDoItem = req.body.item;
+    toDoList.push(newToDoItem);
+    res.status(201).send({
+        message: "Task added successfully"
+    });
+});
+
+app.delete("/todos", (req, res) => {
+    // Callback
+    const itemToDelete = req.body.item;
+    toDoList.find((element, index) => {
+        if (element === itemToDelete) {
+            toDoList.splice(index, 1)
         }
-        else {
-            res.writeHead(404);
-        }
-        res.end();
+    });
+
+    res.status(202).send({
+        message: `Deleted "${req.body.item}"`
     })
-    .listen(port, () => {//callback function 
-        //made for third parties to access the server on local 
-        // host using port 
-        console.log(`nodejs server started on port ${port}`)
-    })
-
-/*
-Below Line means that when you make the above server you can communicate with it
-on Link given below
-http://Localhost:8081
-Open above line in any server it will respond with h1 message which is inside write
+});
 
 
+// Just some additional examples
+// app.get("/todos/create")
+// app.post("todos/create")
 
+// if u make other than get,post or delete request
+app.all("/todos", (req, res) => {
+    res.status(501).send()
+})
 
-Now if u want to create shortcut for node server.js u can create in setup
-or configuration file that is package.json in which term known as script
+// If u  want to go to any other route than /todos u will get error 404
+app.all("*", (req, res) => {
+    res.status(404).send()
+})
 
-write this below lines of code in package.JSON and then whenever u want to start the 
-server open terminal and write npm start
-"scripts": {
-    // if u want to give it another shortcut name then start then in terminal
-    // u have to write npm run (Ur shortcut Name)
-    "start": "node server.js"
-  },
-
-*/
+// Now when u want to start Server 
+app.listen(port, () => {
+    // Callback when server starts
+    console.log(`nodejs server started on port ${port}`);
+})
---
+++ with-nodejs.js
@@ -0,0 +1,96 @@
+//Package import-->this http package means u need a server of 
+// http type and then we r storing this package in a variable 
+// for further use 
+const http = require('http');
+
+// This server will communicate at port 8081
+const port = 8081;//Local port number
+
+// It is list we want to print 
+const toDOList = ["Complete Node Byte", "Play Cricket"];
+http
+    .createServer((req, res) => {
+        const { method, url } = req;
+        if (url === "/todos") {
+            if (method === "GET") {
+                res.writeHead(200);
+                res.write(toDOList.toString());
+
+            }
+            else if (method === "POST") {
+                let body = "";
+                req
+                    .on('error', (err) => {
+                        console.error(err);
+                    })
+                    .on('data', (chunk) => {//Data travels in form of chunk(pieces of JSON file) not in JSON file so that bugs can be easily fixed also it is also better in terms of speed.
+                        // SO now when we recieve pieces of chunk from browser to mein server we have to assemble them again in so we r storing in body variable below is code for that
+                        body += chunk;
+                    })
+                    .on('end', () => { //After the process is end we copy the body data into JSON File
+                        body = JSON.parse(body);
+                        let newToDo = toDOList;
+                        newToDo.push(body.item);
+                        console.log(newToDo);
+                        res.writeHead(201);
+                    })
+            }
+            else if (method === "DELETE") {
+                let body = "";
+                req
+                    .on('error', (err) => {
+                        console.log(err);
+                    })
+                    .on('data', (chunk) => {
+                        body += chunk;
+                    })
+                    .on('end', () => {
+                        body = JSON.parse(body);
+                        let deletedThis = body.item;
+                        for (let i = 0; i < toDOList.length; i++) {
+                            if (toDOList[i] == deletedThis) {
+                                toDOList.splice(i, 1);
+                                break;
+                            }
+                            res.writeHead(204);
+                        }
+
+                    })
+
+            }
+            else {
+                res.writeHead(501);
+            }
+        }
+        else {
+            res.writeHead(404);
+        }
+        res.end();
+    })
+    .listen(port, () => {//callback function 
+        //made for third parties to access the server on local 
+        // host using port 
+        console.log(`nodejs server started on port ${port}`)
+    })
+
+/*
+Below Line means that when you make the above server you can communicate with it
+on Link given below
+http://Localhost:8081
+Open above line in any server it will respond with h1 message which is inside write
+
+
+
+
+Now if u want to create shortcut for node server.js u can create in setup
+or configuration file that is package.json in which term known as script
+
+write this below lines of code in package.JSON and then whenever u want to start the 
+server open terminal and write npm start
+"scripts": {
+    // if u want to give it another shortcut name then start then in terminal
+    // u have to write npm run (Ur shortcut Name)
+    "start": "node server.js"
+  },
+
+*/
